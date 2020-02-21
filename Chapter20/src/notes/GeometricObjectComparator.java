a6c423930939104ed7b180427aa25264518c2841
404kevnotfound <student@HSiMac4-7.local>
2020-02-21 10:45:37 -0700

    Add notes

---
+++ Chapter20/src/notes/GeometricObjectComparator.java
@@ -0,0 +1,8 @@
+package notes;
+/*
+ * Not including the implementation of this one.
+ */
+
+public class GeometricObjectComparator {
+	
+}
---
+++ Chapter20/src/notes/TestComparator.java
@@ -0,0 +1,8 @@
+package notes;
+/*
+ * Not implementing this one either.
+ */
+
+public class TestComparator {
+
+}
