a11c48c0afa3d137e76f082238d6e177b9af1cf7
Alex <31144589+whacky1542@users.noreply.github.com>
2020-04-26 05:04:01 -0600

    Start 23

---
+++ Chapter23/src/notes/InsertionSort.java
@@ -0,0 +1,20 @@
+package notes;
+
+import java.util.ArrayList;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++)
+			list.add((int) (Math.random() * 9 + 1));
+		System.out.println(list.toString());
+		sort(list);
+		System.out.println(list.toString());
+	}
+	
+	public static ArrayList<Integer> sort(ArrayList<Integer> list) {
+		
+		
+		return list;
+	}
+}
